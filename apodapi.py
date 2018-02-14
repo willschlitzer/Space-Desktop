81ee5395ad1d65ca2e78d7878c8d2cd67d6f9e73
Will Schlitzer <schlitzer90@gmail.com>
2018-02-13 21:14:28 -0500

    Added json url, print the url for the HD version of the APOD

--- apodapi.py
+++ apodapi.py
@@ -1,4 +1,10 @@
 '''Displays the Astronomy Picture of the Day'''
 import apodapi_config
+import requests
+import json
 '''The APOD API URL is https://api.nasa.gov/planetary/apod?api_key=APIKEY'''
-print(apodapi_config.apiurl)
+apiurl = apodapi_config.apiurl
+apod_data = requests.get(apiurl).json()
+#print(apod_data)
+imgurl = apod_data['hdurl']
+print(imgurl)
