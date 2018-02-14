0f77754e31e0e48a5e6e35680cecf48a24f23451
Will Schlitzer <schlitzer90@gmail.com>
2018-02-13 23:06:24 -0500

    APOD file is downloaded and displayed

--- apodapi.py
+++ apodapi.py
@@ -1,10 +1,17 @@
 '''Queries the APOD API for the Astronomy Picture
 of the Day and displays it'''
-import apodapi_config
+
 import requests
-import json
+
+import urllib.request
+from PIL import Image
+
+import apodapi_config
+
 apiurl = 'https://api.nasa.gov/planetary/apod?api_key=' + apodapi_config.apikey
-apod_data = requests.get(apiurl).json()
-#print(apod_data)
-imgurl = apod_data['hdurl']
-print(imgurl)
+apoddata = requests.get(apiurl).json()
+imgurl = apoddata['hdurl']
+file = urllib.request.urlretrieve(imgurl, 'apod.jpg')
+image = Image.open('apod.jpg')
+image.show()
+
