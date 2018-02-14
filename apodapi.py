64871073159b84f05962130638d73f8802cc2515
Will Schlitzer <schlitzer90@gmail.com>
2018-02-13 21:20:36 -0500

    Added API URL to main file, only have key in the apodapi_config.py file

--- apodapi.py
+++ apodapi.py
@@ -3,8 +3,7 @@ of the Day and displays it'''
 import apodapi_config
 import requests
 import json
-'''The APOD API URL is https://api.nasa.gov/planetary/apod?api_key=APIKEY'''
-apiurl = apodapi_config.apiurl
+apiurl = 'https://api.nasa.gov/planetary/apod?api_key=' + apodapi_config.apikey
 apod_data = requests.get(apiurl).json()
 #print(apod_data)
 imgurl = apod_data['hdurl']
