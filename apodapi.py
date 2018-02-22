b407569ca95b0bb7fd5e7c2e68ba5872dc3a1d9c
Will Schlitzer <schlitzer90@gmail.com>
2018-02-21 20:25:56 -0500

    Added if 'name == main' to apodapi.py

--- apodapi.py
+++ apodapi.py
@@ -8,10 +8,14 @@ from PIL import Image
 
 import api_config
 
-apiurl = 'https://api.nasa.gov/planetary/apod?api_key=' + api_config.nasaapikey
-apoddata = requests.get(apiurl).json()
-imgurl = apoddata['hdurl']
-urllib.request.urlretrieve(imgurl, 'apod.jpg')
-image = Image.open('apod.jpg')
-image.show()
+def get_apod():
+    apiurl = 'https://api.nasa.gov/planetary/apod?api_key=' + api_config.nasaapikey
+    apoddata = requests.get(apiurl).json()
+    imgurl = apoddata['hdurl']
+    urllib.request.urlretrieve(imgurl, 'apod.jpg')
+    image = Image.open('apod.jpg')
+    image.show()
+
+if __name__ == '__main__':
+    get_apod()
 
