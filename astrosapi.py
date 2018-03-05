aecd685292e58845c9f1a54ed41951719607737f
Will Schlitzer <schlitzer90@gmail.com>
2018-03-05 07:02:02 -0500

    Created a function in astrosapi.py

--- astrosapi.py
+++ astrosapi.py
@@ -2,11 +2,15 @@
 
 import requests
 
-api_url = 'http://api.open-notify.org/astros.json'
-astros_data = requests.get(api_url).json()
-astros_number = astros_data['number']
-print(astros_number)
-astronauts = astros_data['people']
-print('Astronaut\t\tCraft')
-for i in astronauts:
-    print(i['name'], '\t\t', i['craft'])
+def astros():
+    api_url = 'http://api.open-notify.org/astros.json'
+    astros_data = requests.get(api_url).json()
+    astros_number = astros_data['number']
+    print(astros_number)
+    astronauts = astros_data['people']
+    print('Astronaut\t\tCraft')
+    for i in astronauts:
+        print(i['name'], '\t\t', i['craft'])
+
+if __name__ == '__main__':
+    astros()
