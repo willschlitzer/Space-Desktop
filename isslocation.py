da5fafc9211df04470fd128a3463bd26accb7dc6
Will Schlitzer <schlitzer90@gmail.com>
2018-03-05 07:00:11 -0500

    Added docstring and block comment for isslocation.py

--- isslocation.py
+++ isslocation.py
@@ -1,9 +1,13 @@
+"""Queries the ISS API to determine its latitude and longitude"""
+
 import requests
 import json
 
 def iss_data():
+    """Queries the API"""
     apiurl = 'http://api.open-notify.org/iss-now.json'
     issdata = requests.get(apiurl).json()
+    # Prints the returned ISS data
     print(issdata)
 
 if __name__ == '__main__':
