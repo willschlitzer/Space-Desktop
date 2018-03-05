9778cc15d64741ba543f761b78dba15bfdceaa6f
Will Schlitzer <schlitzer90@gmail.com>
2018-03-05 07:04:47 -0500

    Added a docstring and block comments for astrosapi.py

--- astrosapi.py
+++ astrosapi.py
@@ -3,12 +3,16 @@
 import requests
 
 def astros():
+    """Queries the API"""
     api_url = 'http://api.open-notify.org/astros.json'
     astros_data = requests.get(api_url).json()
+    # Saves and prints the number of people in space
     astros_number = astros_data['number']
     print(astros_number)
     astronauts = astros_data['people']
+    # Prints the header for the table of astronauts and spacecraft
     print('Astronaut\t\tCraft')
+    # Prints the fields of astronauts and their spacecraft
     for i in astronauts:
         print(i['name'], '\t\t', i['craft'])
 
