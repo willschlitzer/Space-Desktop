b2795a4034a3b496ac28f0c79f69375284575475
Will Schlitzer <schlitzer90@gmail.com>
2018-03-05 18:09:39 -0500

    Minor changes to mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -19,19 +19,18 @@ def image_ratio(pil_image, width):
     npimage = np.asarray(pil_image, dtype=np.uint8)
     img_h = npimage.shape[0]
     img_w = npimage.shape[1]
-    print(img_h, img_w)
     height = (img_h/img_w) * width
     return int(round(height, 0))
 
 
-root= tk.Tk()
+root = tk.Tk()
 
 
 root.geometry('1500x700')
-canvas= tk.Canvas(root, width= 1499, height= 699)
+canvas = tk.Canvas(root, width= 1499, height= 699)
 cv = tk.Canvas()
 cv.pack(side='top', fill='both', expand='yes')
-apodimage= image_plotter('apod.jpg')
+apodimage = image_plotter('apod.jpg')
 cv.create_image(20, 30, image = apodimage, anchor='nw')
 curiosityimage = image_plotter('curiositypic.jpg', width = 200)
 cv.create_image(600, 30, image= curiosityimage, anchor='nw')
