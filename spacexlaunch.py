c11ee028de49e84ec246233d96e4bdeb2137b99c
Will Schlitzer <schlitzer90@gmail.com>
2018-03-07 10:50:20 -0500

    Create the docstring and block comments for spacexlaunch.py

--- spacexlaunch.py
+++ spacexlaunch.py
@@ -1,18 +1,26 @@
+"""Queries the r/spacex API for the upcoming launch information,
+and returns the information for the next upcoming launch"""
+
 import requests
 
 
 def next_launch():
+    """Queries the r/spacex API and returns launch info"""
     apiurl = 'https://api.spacexdata.com/v2/launches/upcoming'
     spacexdata = requests.get(apiurl).json()
+    # The first entry in spacexdata is the next upcoming launch
     launch = spacexdata[0]
     launch_date_time_utc = launch['launch_date_utc']
+    # Slices the date and time as two separate strings
     launch_date_utc = launch_date_time_utc[0:10]
     launch_time_utc = launch_date_time_utc[11:]
     launch_site = launch['launch_site']['site_name']
+    # Creates a list of payload IDs in the event of multiple payloads on a single launch
     payloads = []
     for payload in launch['rocket']['second_stage']['payloads']:
         payloads.append(payload['payload_id'])
     rocket_name = launch['rocket']['rocket_name']
+    # The number of times the first stage booster has flown
     first_stage_flight_num = launch['rocket']['first_stage']['cores'][0]['flight']
     return launch_date_utc, launch_time_utc, launch_site, payloads, rocket_name, first_stage_flight_num
 
