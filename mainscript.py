fd2eca7d2cc1bdff9423cfbbb193fc0e0336f2dd
Will Schlitzer <schlitzer90@gmail.com>
2018-03-07 00:00:51 -0500

    mainscript.py now displays the ISS location, and the labels for the APOD and Curioristy Photo are centered

--- mainscript.py
+++ mainscript.py
@@ -41,18 +41,18 @@ canvas = tk.Canvas(root, width= 1499, height= 699)
 cv = tk.Canvas()
 cv.pack(side='top', fill='both', expand='yes')
 apodimage = image_plotter('apod.jpg', width = 600)
-cv.create_text(200,15, text='Astronomy Picture of the Day')
-cv.create_image(20, 30, image = apodimage, anchor='nw')
+cv.create_text(225,15, text='Astronomy Picture of the Day', anchor='nw')
+cv.create_image(20, 35, image = apodimage, anchor='nw')
 curiosityimage = image_plotter('curiositypic.jpg', width = 400)
-cv.create_text(850,15, text='Curiosity Photo')
-cv.create_image(650, 30, image= curiosityimage, anchor='nw')
+cv.create_text(800,15, text='Curiosity Photo', anchor = 'nw')
+cv.create_image(650, 35, image= curiosityimage, anchor='nw')
 # Creates the reference points for the People in Space API data
 x1 = 650
 x2 = x1 + 150
-x3 = x2 + 60
 y1 = 450
 y2 = y1 + 10
-# Prints the header titles
+
+# Prints the People in Space header titles
 cv.create_text(x1 + 5, y2 + 5, text='Name', anchor = 'nw')
 cv.create_text(x2 + 5, y2 + 5, text='Craft', anchor = 'nw')
 # Prints the names and spacecraft of the individuals in space
@@ -62,5 +62,13 @@ for astro in astronauts:
     astrocraft = astro['craft']
     cv.create_text(x1 + 5, y2, text=astroname, anchor = 'nw')
     cv.create_text(x2 + 5, y2, text=astrocraft, anchor = 'nw')
+
+# The ISS location data and the current time of the request
+issdata, date_time = isslocation.iss_data()
+iss_lat, iss_long = issdata['iss_position']['latitude'], issdata['iss_position']['longitude']
+cv.create_text(1100, 15, text='ISS Location', anchor='nw')
+cv.create_text(1100, 30, text='Date: ' + str(date_time), anchor='nw')
+cv.create_text(1100, 45, text='Latitude: ' + str(iss_lat), anchor='nw')
+cv.create_text(1100, 60, text='Longitude: ' + str(iss_long), anchor='nw')
 
 root.mainloop()
