1952196a36c086141e9b4e3ea0225a88cc5e7956
Will Schlitzer <schlitzer90@gmail.com>
2018-03-07 10:44:33 -0500

    Assigned variables to the  elements of the .json data for the next launch, and created a return statement.

--- spacexlaunch.py
+++ spacexlaunch.py
@@ -1,5 +1,21 @@
 import requests
 
-apiurl = 'https://api.spacexdata.com/v2/launches/upcoming'
-spacexdata = requests.get(apiurl).json()
-print(spacexdata)
+
+def next_launch():
+    apiurl = 'https://api.spacexdata.com/v2/launches/upcoming'
+    spacexdata = requests.get(apiurl).json()
+    launch = spacexdata[0]
+    launch_date_time_utc = launch['launch_date_utc']
+    launch_date_utc = launch_date_time_utc[0:10]
+    launch_time_utc = launch_date_time_utc[11:]
+    launch_site = launch['launch_site']['site_name']
+    payloads = []
+    for payload in launch['rocket']['second_stage']['payloads']:
+        payloads.append(payload['payload_id'])
+    rocket_name = launch['rocket']['rocket_name']
+    first_stage_flight_num = launch['rocket']['first_stage']['cores'][0]['flight']
+    return launch_date_utc, launch_time_utc, launch_site, payloads, rocket_name, first_stage_flight_num
+
+
+if __name__ == '__main__':
+    print(next_launch())
