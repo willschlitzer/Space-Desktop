4f337ce11dd8cc46f6f36d57ecc465a153446267
Will Schlitzer <schlitzer90@gmail.com>
2018-03-19 22:26:15 -0400

    Updated isslocation.py to return a dictionary of the date, time, latitude, and longitude

--- isslocation.py
+++ isslocation.py
@@ -8,7 +8,11 @@ def iss_data():
     apiurl = 'http://api.open-notify.org/iss-now.json'
     issdata = requests.get(apiurl).json()
     # Prints the returned ISS data
-    return issdata, datetime.datetime.utcnow()
+    utcnow = str(datetime.datetime.utcnow())
+    return {'lat': round(float(issdata['iss_position']['latitude']),2),
+            'long': round(float(issdata['iss_position']['latitude']),2),
+            'date': utcnow[:10],
+            'time': utcnow[11:19]}
 
 if __name__ == '__main__':
     print(iss_data())
